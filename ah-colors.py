bdb5afb9f1d4d079798130474c044582281842a9
ah205 <91565851+ah205@users.noreply.github.com>
2021-10-04 15:10:14 -0500

    Add files via upload
    Colors assignment

---
+++ ah-colors.py
@@ -0,0 +1,14 @@
+#Initialization
+import turtle
+alex = turtle.Turtle()
+import random
+
+#Functions
+def randomDot():
+    alex.color("yellow")
+    alex.begin_fill()
+    alex.circle(50)
+    alex.end_fill()
+
+#Main
+randomDot()
